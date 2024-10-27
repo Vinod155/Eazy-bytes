b7d6c49f2daeacf9e3c9a98c6c754a7e3ed4132d
unknown <thakurr2v@gmail.com>
2024-10-27 09:35:32 +0530

    adding upto section 6

---
+++ V5.EazyBytesEdgeServer/eurekaserver/src/main/java/com/example/eurekaserver/EurekaserverApplication.java
@@ -0,0 +1,16 @@
+package com.example.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+//this annotation we convert normal project to act as service discovery agent with the help of eureka
+public class  EurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
+
+}
